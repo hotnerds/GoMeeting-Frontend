7243df640e6ca49bf34d84d463e50aeeba9044ce
jungin0507 <a01033046758@gmail.com>
2021-11-12 22:53:46 +0900

    feat: home routes

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Auth from './routes/Auth';
 import UserSignUp from './components/UserSignUp';
+import Home from './routes/Home';
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route expact path="/"></Route>
+        <Route expact path="/" element={<Home />}></Route>
         <Route expact path="/auth" element={<Auth />}></Route>
         <Route expact path="/register" element={<UserSignUp />}></Route>
       </Routes>
---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import HomeMyInfo from '../components/HomeMyInfo';
+const Home = () => {
+  return (
+    <section className="navigatior">
+      <Navigation />
+      <HomeMyInfo />
+    </section>
+  );
+};
+
+export default Home;
