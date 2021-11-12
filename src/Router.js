bb10332cca809fa0edc10f85ae1102ca3d471c0c
jungin0507 <a01033046758@gmail.com>
2021-11-12 14:52:48 +0900

    feat: create router component

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route expact path="/"></Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
